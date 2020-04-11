c2cf275cb42e1350f16a5d48962e23e77373a3f8
BlackText <r.cojan@yahoo.com>
2020-04-12 02:10:46 +0300

    Added trees!

--- Prototype/Prototype/Prototype.cpp
+++ Prototype/Prototype/Prototype.cpp
@@ -3,19 +3,61 @@
 
 #include "pch.h"
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class Tree
+{
+public:
+	int ring_count;
+	int bark_count;
+	int wood_count;
+
+	virtual int get_resources() = 0;
+};
+
+class Fruit_bearing : Tree 
+{
+public:
+	string name_type;
+	int fruit_count;
+
+	int get_resources() 
+	{
+	
+		int resources = ring_count + bark_count + wood_count + fruit_count;
+		return resources;
+
+	};
+
+};
+
+class Conifer : Tree
+{
+public:
+	string name_type;
+	int cone_count;
+
+	int get_resources() 
+	{
+		int resources = ring_count + bark_count + wood_count + cone_count;
+		return resources;
+
+	};
+
+};
+
+class Appletree : Fruit_bearing
+{
+
+
+
+
+};
+
 
 int main()
 {
     std::cout << "Hello World!\n"; 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
